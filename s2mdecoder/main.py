7ab40aabd6aaeb23dd19e0f6902efbab72f15001
Talv <krz.krawczuk@gmail.com>
2020-08-09 22:10:40 +0200

    game attributes & variants

--- s2mdecoder/main.py
+++ s2mdecoder/main.py
@@ -9,6 +9,10 @@ from sc2reader.decoders import BitPackedDecoder
 def to_hex(data):
     return binascii.hexlify(data).decode('ascii')
 
+def to_bool(data: int):
+    assert 0 <= data <= 1
+    return bool(data)
+
 def read_depot_link(data):
     o = {}
     o['type'] = data[0:4].decode('ascii')
@@ -35,13 +39,13 @@ def read_localization_table_key(data):
     if data is None:
         return None
     assert len(data) == 3
-    assert data[0] is None
     return {
+        'color': data[0],
         'table': data[1],
         'index': data[2],
     }
 
-def read_screenshot_image(data):
+def read_picture(data):
     if data is None:
         return None
     assert len(data) == 5
@@ -49,7 +53,6 @@ def read_screenshot_image(data):
         'index': data[0],
         'top': data[1],
         'left': data[2],
-        # or bottom & right?
         'width': data[3],
         'height': data[4],
     }
@@ -57,8 +60,15 @@ def read_screenshot_image(data):
 def read_screenshot_entry(data):
     assert len(data) == 2
     return {
-        'picture': read_screenshot_image(data[0]),
+        'picture': read_picture(data[0]),
         'caption': read_localization_table_key(data[1]),
+    }
+
+def read_namespace_definition(data):
+    assert len(data) == 2
+    return {
+        'namespace': read_attribute_link(data[0]),
+        'value': read_attribute_default_value_or_values(data[1]),
     }
 
 def read_map_info(data):
@@ -70,18 +80,15 @@ def read_map_info(data):
     o['name'] = read_localization_table_key(data[0])
     o['description'] = read_localization_table_key(data[1])
     assert data[2] == None
-    o['thumbnail'] = read_screenshot_image(data[3])
+    o['thumbnail'] = read_picture(data[3])
     o['maxPlayers'] = data[4]
-    # 5 specialScope / flags ??
     assert data[5] == 22
-    # TODO:
-    # o['instances'] = data[6]
+    o['namespaceDefinitions'] = [*map(read_namespace_definition, data[6])]
     o['visualFiles'] = [*map(read_depot_link, data[7])]
     o['localeTable'] = [*map(read_localization_link, data[8])]
 
-    if ver > 8:
-        assert len(data[9]) == 0
     if ver >= 10:
+        assert len(data[9]) == 0
         assert len(data[10]) == 0
     if ver >= 11:
         # 11: [{0: {0: 999, 1: 3004}, 1: 1011, 2: [{0: b'\x00Lic', 1: 162}]}]}
@@ -96,23 +103,111 @@ def read_premium_info(data):
     o['license'] = data[0]
     return o
 
+def read_attribute_visual(data):
+    assert len(data) == 3
+    return {
+        'text': read_localization_table_key(data[0]),
+        'tip': read_localization_table_key(data[1]),
+        'art': read_picture(data[2]),
+    }
+
+def read_attribute_value_definition(data):
+    ver = max(data.keys())
+    assert ver in [1, 2]
+    if ver >= 2:
+        assert len(data[2]) == 0
+    return {
+        'value': data[0].decode('ascii'),
+        'visual': read_attribute_visual(data[1])
+    }
+
+VISIBILITY_TYPE = {
+    0: 'none',
+    1: 'self',
+    2: 'host',
+    3: 'all',
+}
+
+def read_attribute_definition(data):
+    o = {}
+    o['namespace'] = read_attribute_link(data[0])
+    o['values'] = [*map(read_attribute_value_definition, data[1])]
+    o['visual'] = read_attribute_visual(data[2])
+    o['requirements'] = str(data[3])
+    o['arbitration'] = data[4]
+    # 0x00 always
+    # 0x01 first come, first serve
+    o['visibility'] = VISIBILITY_TYPE[data[5]]
+    o['access'] = VISIBILITY_TYPE[data[6]]
+    o['options'] = data[7]
+    # options flags:
+    # 0x01 unknown
+    # 0x02 locked when public
+    # 0x04 hidden
+    o['default'] = read_attribute_default_value_or_values(data[8])
+    o['sortOrder'] = data[9]
+    return o
+
+def read_attribute_link(data):
+    assert len(data) == 2
+    return {
+        'namespace': data[0],
+        'id': data[1],
+    }
+
+def read_attribute_default_value(data):
+    assert len(data) == 2
+    return {
+        'index': data[0],
+        'unk_attr_val_1': data[1],
+    }
+
+def read_attribute_default_value_or_values(data):
+    if isinstance(data, list):
+        return [*map(read_attribute_default_value, data)]
+    else:
+        return read_attribute_default_value(data)
+
+def read_variant_attribute_defaults(data):
+    assert len(data) == 2
+    o = {}
+    o['attribute'] = read_attribute_link(data[0])
+    o['value'] = read_attribute_default_value_or_values(data[1])
+    return o
+
+def read_variant_attribute_locked(data):
+    assert len(data) == 2
+    o = {}
+    o['attribute'] = read_attribute_link(data[0])
+    o['lockedScopes'] = data[1]
+    return o
+
+def read_variant_attribute_visibility(data):
+    assert len(data) == 2
+    o = {}
+    o['attribute'] = read_attribute_link(data[0])
+    o['hidden'] = data[1]
+    return o
+
 def read_variant_info(data):
     o = {}
     ver = max(data.keys())
     assert ver in [8, 11, 12, 13, 14, 15]
+    assert len(data[0]) == 2
+    o['categoryId'] = data[0][0]
+    o['modeId'] = data[0][1]
     o['categoryName'] = read_localization_table_key(data[1])
     o['modeName'] = read_localization_table_key(data[2])
     o['categoryDescription'] = read_localization_table_key(data[3])
     o['modeDescription'] = read_localization_table_key(data[4])
-    # TODO: 6 attribute defaults?
-    # TODO: 7 attribute namespace?
-    o['categoryId'] = data[8]
-    if ver >= 9:
-        # TODO:
-        # o['lockedAttributes'] = data[9]
-        # o['instanceRestrictions'] = data[10]
-        pass
+    assert len(data[5]) == 3
+    o['attributeDefaults'] = [*map(read_variant_attribute_defaults, data[6])]
+    o['lockedAttributes'] = [*map(read_variant_attribute_locked, data[7])]
+    o['maxTeamSize'] = data[8]
     if ver >= 11:
+        o['attributeVisibility'] = [*map(read_variant_attribute_visibility, data[9])]
+        # TODO: data[10] - attribute value restrictions?
+        # 10: [{0: {0: 999, 1: 500}, 1: {0: [8, 8, 8, 8]}}],
         o['achievementTags'] = [x.strip(b'\x00').decode('ascii') for x in data[11]]
     if ver >= 12:
         o['maxHumanPlayers'] = data[12]
@@ -124,21 +219,38 @@ def read_variant_info(data):
         o['teamNames'] = [*map(read_localization_table_key, data[15])]
     return o
 
+LIST_TYPE = {
+    0: 'bulleted',
+    1: 'numbered',
+    2: 'none',
+}
+
 def read_arcade_section_header(data):
     assert len(data) == 4
     o = {}
     o['title'] = read_localization_table_key(data[0])
     o['startOffset'] = data[1]
-    o['listType'] = data[2]
+    o['listType'] = LIST_TYPE[data[2]]
     o['subtitle'] = read_localization_table_key(data[3])
     return o
 
-def read_arcade_section(data):
+def read_arcade_section_raw(data):
     assert len(data) == 2
     o = {}
     o['headers'] = [*map(read_arcade_section_header, data[0])]
     o['items'] = [*map(read_localization_table_key, data[1])]
     return o
+
+def read_arcade_section(data):
+    assert len(data) == 2
+    sect_headers = [*map(read_arcade_section_header, data[0])]
+    sect_items = [*map(read_localization_table_key, data[1])]
+    prev = len(sect_items)
+    for x in reversed(sect_headers):
+        x['items'] = sect_items[x['startOffset']:prev]
+        prev = x['startOffset']
+        del x['startOffset']
+    return sect_headers
 
 def read_arcade_tutorial_link(data):
     assert len(data) == 3
@@ -163,9 +275,8 @@ def read_arcade_info(data):
     o['howToPlayScreenshots'] = [*map(read_screenshot_entry, data[3])]
     o['howToPlaySections'] = read_arcade_section(data[4])
     o['patchNoteSections'] = read_arcade_section(data[5])
-    o['mapIcon'] = read_screenshot_image(data[6])
+    o['mapIcon'] = read_picture(data[6])
     o['tutorialLink'] = read_arcade_tutorial_link(data[7]) if data[7] is not None else None
-    # 7: {0: 1, 1: b'Fasr', 2: [{0: {0: 107900, 1: 131271}, 1: 0}]},
     o['matchmakerTags'] = [x.strip(b'\x00').decode('ascii') for x in data[8]]
     o['website'] = read_localization_table_key(data[9])
 
@@ -180,18 +291,21 @@ def read_s2mh(data):
     ver = max(data.keys())
     assert ver in [13, 14, 18, 22, 23, 24]
 
+    # pprint(data, width=200)
+
     assert len(data[0]) == 2
-    o['instance'] = read_instance_header(data[0])
+    o['header'] = read_instance_header(data[0])
     o['name'] = data[1].decode('utf8')
     o['mapFile'] = read_depot_link(data[2])
     o['mapNamespace'] = data[3]
     o['mapInfo'] = read_map_info(data[4])
+    o['attributes'] = [*map(read_attribute_definition, data[5])]
 
-    # TODO: 5 - game attributes etc.
-    o['definitions'] = []
+    if 6 in data:
+        o['unk6'] = '%s' % data[6]
 
     # TODO: 7 - score IDs and such?
-    o['resultDefinitions'] = []
+    # o['resultDefinitions'] = []
 
     o['localeTable'] = [*map(read_localization_link, data[8])]
     o['mapSize'] = {
@@ -208,12 +322,12 @@ def read_s2mh(data):
     if ver >= 14:
         o['dependencies'] = [*map(read_instance_header, data[14])]
     if ver >= 18:
-        o['unknown_15'] = data[15]
-        o['licenses'] = [*map(lambda x: {'name': x[0].strip(b'\x00').decode('ascii'), 'number': x[1]}, data[16])]
+        o['addDefaultPermissions'] = to_bool(data[15])
+        o['relevantPermissions'] = [*map(lambda x: {'name': x[0].strip(b'\x00').decode('ascii'), 'number': x[1]}, data[16])]
         o['specialTags'] = [x.strip(b'\x00').decode('ascii') for x in data[18]]
     if ver >= 22:
         o['arcade'] = read_arcade_info(data[19]) if data[19] else None
-        o['unknown_22'] = data[22]
+        o['addMultiMod'] = to_bool(data[22])
     if ver >= 23:
         # 23: [b'SC2ParkVoicePack'
         pass
@@ -255,17 +369,13 @@ def s2mh_apply_s2ml(data, translation, fields = None):
                     'caption': True,
                 },
                 'howToPlaySections': {
-                    'headers': {
-                        'title': True,
-                        'subtitle': True,
-                    },
+                    'title': True,
+                    'subtitle': True,
                     'items': True,
                 },
                 'patchNoteSections': {
-                    'headers': {
-                        'title': True,
-                        'subtitle': True,
-                    },
+                    'title': True,
+                    'subtitle': True,
                     'items': True,
                 },
                 'website': True,
@@ -277,12 +387,12 @@ def s2mh_apply_s2ml(data, translation, fields = None):
             for y, item in enumerate(data[x]):
                 if fields[x] is True:
                     data[x][y] = translate_prop(data[x][y])
-                else:
+                elif isinstance(fields[x], dict):
                     s2mh_apply_s2ml(data[x][y], translation, fields[x])
         else:
-            if fields[x] is True:
+            if fields[x] is True and data[x] is not None:
                 data[x] = translate_prop(data[x])
-            else:
+            elif isinstance(fields[x], dict):
                 s2mh_apply_s2ml(data[x], translation, fields[x])
 
     return data
@@ -295,7 +405,7 @@ def read_s2mi(data):
     ver = max(data.keys())
     assert ver in [22, 23, 26]
 
-    o['instance'] = read_instance_header(data[0])
+    o['header'] = read_instance_header(data[0])
     o['headerCacheHandle'] = read_depot_link(data[1])
     o['uploadTime'] = data[2]
     o['isLinked'] = bool(data[3])
@@ -343,7 +453,7 @@ def read_s2ml(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     for child in root:
-        o[child.attrib['id']] = child.text
+        o[child.attrib['id']] = child.text or ''
     return o
 
 
